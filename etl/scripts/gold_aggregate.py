e3c1373c6275251e6e91e1839a5fa0cec67aaa2e
UsmanAdnan234 <usmankhanxyz12345@gmail.com>
2026-06-10 01:20:12 +0500

    fix: sort imports in ETL scripts

--- etl/scripts/gold_aggregate.py
+++ etl/scripts/gold_aggregate.py
@@ -10,7 +10,7 @@ from io import BytesIO
 
 import boto3
 import pandas as pd
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql import functions as F
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
